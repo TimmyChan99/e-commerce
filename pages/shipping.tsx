7ebd95bd8cb0f31aa5462da355531d6ab3363a4d
Fatima Ezzahra <felemenoun@gmail.com>
2022-11-17 20:45:05 +0100

    Create shipping page

---
+++ pages/shipping.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const shipping = () => {
+	return (
+		<Layout title='Shipping'>
+			<h1>Shipping</h1>
+		</Layout>
+	)
+}
+
+export default shipping
