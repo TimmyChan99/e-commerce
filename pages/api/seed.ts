c95a9a36e076113727c5c4a27ee78d62068ea17c
Fatima Ezzahra <felemenoun@gmail.com>
2022-12-02 20:16:48 +0100

    Create Products seed

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Product from "../../models/Product";
 import User from "../../models/User";
 import data from "../../utils/data";
 import dbConnect from "../../utils/db";
@@ -7,6 +8,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	await dbConnect();
   await User.deleteMany({});
 	await User.insertMany(data.users);
+	await Product.deleteMany({});
+	await Product.insertMany(data.products);
 	res.status(200).json({ message: "Data imported successfully" });
 };
 
